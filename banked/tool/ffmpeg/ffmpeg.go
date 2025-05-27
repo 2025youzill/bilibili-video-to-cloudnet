355aa2207921b666dcf0466db469dbb28c9bdf29
2025youzill <1307474985@qq.com>
2025-05-27 21:18:20 +0800

    删除ffprobe

--- banked/tool/ffmpeg/ffmpeg.go
+++ banked/tool/ffmpeg/ffmpeg.go
@@ -12,7 +12,6 @@ import (
 //go:embed ffmpeg.exe
 var ffmpegBin []byte
 
-
 // 生成随机字符串
 func randomString(n int) string {
 	// 1. 创建独立随机源（避免全局锁，并发安全）
@@ -31,13 +30,12 @@ func randomString(n int) string {
 }
 
 // 提取 FFmpeg 到临时文件
-func ExtractFFmpeg() (string, string, error) {
+func ExtractFFmpeg() (string, error) {
 	// 生成唯一的临时文件名（避免多用户冲突）
 	ffmpegTmpExe := filepath.Join(os.TempDir(), "ffmpeg_"+randomString(8)+".exe")
-	ffprobeTmpExe := filepath.Join(os.TempDir(), "ffprobe_"+randomString(8)+".exe")
 	// 写入二进制数据
 	if err := os.WriteFile(ffmpegTmpExe, ffmpegBin, 0755); err != nil {
-		return "", "", fmt.Errorf("写入临时文件失败: %v", err)
+		return "", fmt.Errorf("写入临时文件失败: %v", err)
 	}
-	return ffmpegTmpExe, ffprobeTmpExe, nil
+	return ffmpegTmpExe, nil
 }
