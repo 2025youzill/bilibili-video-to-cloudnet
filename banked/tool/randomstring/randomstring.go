48ca2efbeb2b598b5e922d609e1b1a223a827208
2025youzill <1307474985@qq.com>
2025-07-05 11:55:32 +0800

    fix: 修改同一时间生成字符串相同问题

--- banked/tool/randomstring/randomstring.go
+++ banked/tool/randomstring/randomstring.go
@@ -2,15 +2,29 @@ package randomstring
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
+var (
+	globalRand *rand.Rand
+	initOnce   sync.Once
+)
+
+func init() {
+	// 利用 init 函数确保初始化（线程安全）
+	initGlobalRand()
+}
+
+func initGlobalRand() {
+	globalRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
 func GenerateRandomString(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+		b[i] = charset[globalRand.Intn(len(charset))]
 	}
 	return string(b)
 }
